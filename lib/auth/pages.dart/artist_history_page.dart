689e4827412773f6695c6016c3d51b3a06e5b7be
marya alsubaie <99149167+MaryaQing@users.noreply.github.com>
2026-03-10 02:53:02 +0300

    Modify text and style in artist history page
    Updated text content and styles for better readability.

--- lib/auth/pages.dart/artist_history_page.dart
+++ lib/auth/pages.dart/artist_history_page.dart
@@ -105,7 +105,7 @@ artists = data
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Artists in History",
+                "\n Artists in History",
                 style: TextStyle(
                   fontSize: width < 600 ? 26 : 48,
                   fontFamily: "Serif",
@@ -116,12 +116,12 @@ artists = data
               const SizedBox(height: 15),
               const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  "Explore the legacy of those who shaped the art world across centuries.",
+                  child: Text(
+                  "\n Explore the legacy of those who shaped the art world\n across centuries.",
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white70,
+                    fontSize: 24,
+                    color:Color.fromARGB(255, 235, 184, 30),
                   ),
                 ),
               ),
